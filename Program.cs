81737f1e875b7412a9eba0b1a96f573f16cac01e
Dennis Bonesso <dennisbonesso@gmail.com>
2022-11-04 10:56:44 -0300

    Exercico pronto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia1 = new Nokia("123","abc","dcd", 123);
+nokia1.InstalarAplicativo("Uber");
+nokia1.Ligar();
+
+Iphone iphone1 = new Iphone("1234","abcd","dcdc", 1234);
+iphone1.InstalarAplicativo("Ifood");
+iphone1.ReceberLigacao();
